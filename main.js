5c5a51195461978074d98c2d41eeb15ea9240d64
jeffbalagosa <61676139+jeffbalagosa@users.noreply.github.com>
2021-10-26 20:52:40 -0700

    Working on #4

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -13,10 +13,21 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (num, dnaArray) => {
+  return {
+    specimentNum: num,
+    dna: dnaArray,
+    mutate() {
+      const newBase = returnRandBase();
+      const dnaIndex = dnaArray.indexOf(
+        Math.floor(Math.random() * dnaArray.length)
+      );
+      if (dnaIndex !== -1) {
+        dnaArray[dnaIndex] = newBase;
+      }
+      return dnaArray;
+    },
+  };
+};
 
-
-
-
-
-
-
+console.log(pAequorFactory(1, mockUpStrand()));
